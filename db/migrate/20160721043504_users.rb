2c27680e56e5a5b33876a6bbb03d6656d1316369
Zhezhang Fu <zephitez@gmail.com>
2016-07-24 17:34:28 +0800

    fixing register new user

--- db/migrate/20160721043504_users.rb
+++ db/migrate/20160721043504_users.rb
@@ -1,11 +1,11 @@
 class Users < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string  :name, null: false, limit: 20
-      t.string  :email, null: false
+      t.string  :name
+      t.string  :email
       t.string  :address, null: false
-      t.string  :password, null: false
-      t.integer :cc_number, null: false
+      t.string  :password
+      t.integer :cc_number
 
       t.timestamps
     end
